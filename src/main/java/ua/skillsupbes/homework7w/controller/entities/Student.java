9bf5954bb336e49fd0c917cbd93144cff8800fa5
Andr <you@example.com>
2014-08-05 20:06:10 +0300

    adjust

--- src/main/java/ua/skillsupbes/homework7w/controller/entities/Student.java
+++ src/main/java/ua/skillsupbes/homework7w/controller/entities/Student.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+exemple https://github.com/logart-skillsup/java1-hibernate.git
  */
 package ua.skillsupbes.homework7w.controller.entities;
 
